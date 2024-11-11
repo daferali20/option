6b9edfbc644ce25a7f399283cc813b0d9dfe4fea
daferali20 <31751340+daferali20@users.noreply.github.com>
2024-11-11 09:04:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // دالة لجلب بيانات عقود الأوبشن باستخدام Alpha Vantage
 async function fetchOptionsData() {
     const proxyUrl = "https://corsproxy.io/?"; // خادم وسيط لتجاوز قيود CORS
-    //const apiUrl = "https://www.alphavantage.co/query?function=OPTION_CHAIN&symbol=TSLA&apikey=26E4LN8EWHQHLJSG";
+    const apiUrl = "https://www.alphavantage.co/query?function=OPTION_CHAIN&symbol=TSLA&apikey=26E4LN8EWHQHLJSG";
     const url = 'https://corsproxy.io/?' + encodeURIComponent('https://www.alphavantage.co/query?function=OPTION_CHAIN&symbol=TSLA&apikey=26E4LN8EWHQHLJSG');
 
     try {
